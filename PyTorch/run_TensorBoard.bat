1d490ecc576e083f058b2daf300674c09e46c745
Seung-Tak Noh <noh@ui.is.s.u-tokyo.ac.jp>
2020-07-15 12:16:23 +0900

    # improve run_TensorBoard.bat
    - open chrome from the command line

--- PyTorch/run_TensorBoard.bat
+++ PyTorch/run_TensorBoard.bat
@@ -2,4 +2,5 @@
 set PATH=%PATH%;C:\ProgramData\Anaconda3\Scripts
 CALL C:\ProgramData\Anaconda3\Scripts\activate.bat venv_pt
 START tensorboard --logdir=./runs/fashion_mnist_experiment_1 --port 6006
+START chrome /new-tab http://localhost:6006
 EXIT
